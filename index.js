e45304a3d4c1afa4589fcd6ab16c4c211aca5892
NunoGomes <nunomrgomes@gmail.com>
2022-01-13 19:23:57 +0000

    feat: extension adds a control to the actual web page

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-const setSpeed = document.getElementById("set-speed");
+chrome.tabs.query({ active: true, currentWindow: true }).then(tabs => {
+  const [tab] = tabs
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {
+      if(document.getElementById('youtube-accelerator-input')) return
 
-setSpeed.addEventListener("click", async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      const { hostname, pathname } = window.location
 
-  if(tab.url.startsWith("https://www.youtube.com")) {
-    var playbackSpeed = document.getElementById("playback-speed").value;
-    
-    await chrome.storage.sync.set({ playbackSpeed });
-    
-    document.getElementById("current-speed").innerHTML = `Playing at ${playbackSpeed}x speed`
-    
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: () => {        
-        chrome.storage.sync.get("playbackSpeed", ({ playbackSpeed }) => {
-          const videoPlayer = document.getElementsByClassName('video-stream html5-main-video')[0]
-          videoPlayer.playbackRate = Number(playbackSpeed)
-        });
-      }      
-    });    
-  }
+      if(hostname === "www.youtube.com" && pathname === "/watch") {
+        // Video
+        const video = document.querySelector('video.video-stream.html5-main-video')
+        
+        // Input Element
+        const input = document.createElement('input')
+        // Attributes
+        input.id = 'youtube-accelerator-input'
+        input.type = 'number'
+        input.min = '0.1'
+        input.max = '16'
+        input.step = '0.1'
+        // Style
+        input.style.height = '1.3em'
+        input.style.width = '3.6em'
+        input.style.borderStyle = 'groove'
+        input.style.borderRadius = '0'
+        input.style.outline = 'none'
+        
+        // Button Element
+        const button = document.createElement('button')
+        // Attributes
+        button.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M13,6V18L21.5,12M4,18L12.5,12L4,6V18Z" />
+          </svg>`
+        // Style
+        button.style.border = 'none'
+        button.style.cursor = 'pointer'
+        button.style.backgroundColor = 'transparent'
 
-});
+        input.value = video.playbackRate
+
+
+        button.addEventListener('click', () => {
+          const input = document.getElementById('youtube-accelerator-input')
+          const video = document.querySelector('video.video-stream.html5-main-video')
+
+          video.playbackRate = Number(input.value) || 1
+        })
+        const menuContainer = document.getElementById('menu-container')
+
+        menuContainer.parentElement.insertBefore(input, menuContainer.nextSibling)
+        menuContainer.parentElement.insertBefore(button, input.nextSibling)
+      }
+    }   
+  })
+})
