a1388c8cff306b0e7ea8b436465f51b90a5a7ee9
NunoGomes <nunomrgomes@gmail.com>
2022-01-29 03:37:49 +0000

    chore: webpack script to build project + new minor version

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,37 @@
-const path = require('path');
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 function modify(buffer) {
-    // copy-webpack-plugin passes a buffer
-    var manifest = JSON.parse(buffer.toString());
- 
-    // make any modifications you like, such as
-    manifest.name = "Youtube Accelerator";
- 
-    // pretty print to JSON with two spaces
-    manifest_JSON = JSON.stringify(manifest, null, 2);
-    return manifest_JSON;
+  // copy-webpack-plugin passes a buffer
+  var manifest = JSON.parse(buffer.toString());
+
+  // make any modifications you like, such as
+  manifest.name = "Youtube Accelerator";
+  manifest_JSON = JSON.stringify(manifest, null, 2);
+  return manifest_JSON;
 }
- 
+
 module.exports = {
-    entry: {
-        "youtube/homepage.js": './youtube/homepage.js',
-        "youtube/video.js": './youtube/video.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name]',
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.js$/,
-                exclude: '/node_modules/', 
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                        ]
-                    }
-                } 
-            },
-        ],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { 
-                    from: "./manifest.json",
-                    transform (content, path) {
-                        return modify(content)
-                    }
-                },
-                { from: "assets/icons/", to: "assets/icons/" },
-            ],
-        })
-    ,],
+  entry: {
+    "youtube/homepage": ["./youtube/homepage.js"],
+    "youtube/video": ["./youtube/video.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./manifest.json",
+          transform(content, path) {
+            return modify(content);
+          },
+        },
+        { from: "assets/icons/", to: "assets/icons/" },
+        { from: "youtube/utils.js", to: "youtube/utils.js" },
+      ],
+    }),
+  ],
 };
